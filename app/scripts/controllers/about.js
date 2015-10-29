4482adca8531b7fd42b8b3ba9233968169c3129a
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-29 22:49:54 +0100

    created basic UI

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dockerToolsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dockerToolsApp
+ */
+angular.module('dockerToolsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dockerToolsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dockerToolsApp
+ */
+angular.module('dockerToolsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
